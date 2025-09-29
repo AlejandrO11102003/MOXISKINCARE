2cb079cc4dc81d499401528ef250401100d82b68
AlejandrO11102003 <u22239328@utp.edu.pe>
2025-09-29 11:55:56 -0500

    feat: implementacion vista usuarios

--- demo/src/main/java/com/example/demo/Controller/HolaController.java
+++ demo/src/main/java/com/example/demo/Controller/HolaController.java
@@ -1,14 +1,14 @@
 package com.example.demo.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HolaController {
 
-    @GetMapping("/hola")
-    public String hola() {
-        return "Hola, mundo!";
+    @GetMapping("/Usuarios")
+    public String user() {
+        return "usuarios";
     }
 
 }
--- demo/src/main/java/com/example/demo/Repository/UsuarioRepository.java
+++ demo/src/main/java/com/example/demo/Repository/UsuarioRepository.java
@@ -1,8 +1,9 @@
 package com.example.demo.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.example.demo.Entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
- 
+
 }
