defc4887f2304fbc3810d6122c0a951f6e9d27af
AlejandrO11102003 <u22239328@utp.edu.pe>
2025-10-15 21:55:09 -0500

    fix: correcion de estructura

--- demo/src/main/java/com/example/Repository/UsuarioRepository.java
+++ demo/src/main/java/com/example/Repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.Entity.Usuario;
 
 public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
- 
+
 }
