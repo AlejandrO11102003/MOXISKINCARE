e94c8ee42eb72fe36a6f4167ce69aac1f97998b4
AlejandrO11102003 <u22239328@utp.edu.pe>
2025-11-13 13:20:41 -0500

    xd

--- demo/src/test/java/com/example/demo/DemoApplicationTests.java
+++ demo/src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,13 +1,27 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // Enable full stack trace
+        System.out.println("Context loading...");
+    }
 
+    @Test
+    void contextLoadsWithDebug() {
+        try (ConfigurableApplicationContext context = 
+             new SpringApplicationBuilder(DemoApplication.class)
+                 .profiles("test")
+                 .run()) {
+        }
+    }
 }
